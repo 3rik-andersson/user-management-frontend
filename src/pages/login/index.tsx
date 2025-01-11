b2796765d7f140be31b7ad37def43a02c65da597
Reid <reid.al.harrisson@gmail.com>
2025-01-11 19:03:18 +0100

    feat: fix login responsive design

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,29 +1,32 @@
-import "./style.scss"
-import { Input } from "../../components/input"
-import { Link } from "../../components/link"
-import { Button } from "../../components/button"
-import { Logo } from "../../components/logo"
+import "./style.scss";
+import { Input } from "../../components/input";
+import { Link } from "../../components/link";
+import { Button } from "../../components/button";
+import LoginImage from "../../assets/image/pablo-sign-in.png";
+import LogoIcon from "../../assets/icon/lensqr.svg";
 
 export const Login = () => {
-    return (
-        <div className="login-container">
-            <div className="login-leftside">
-                <div className="login-logo" ><Logo type="big" /></div>
-                <div className="login-back" />
-            </div>
-            <div className="login-rightside">
-                <div className="login-form">
-                    <h1 className="login-heading">Welcome!</h1>
-                    <p className="login-text">Enter details to login.</p>
-                    <Input placeholder="Email" type="big" />
-                    <div className="login-password">
-                        <Input placeholder="Password" type="big-broken" />
-                        <Link type="broken">SHOW</Link>
-                    </div>
-                    <Link type="normal">FORGOT PASSWORD?</Link>
-                    <Button type="big" url="/users">LOG IN</Button>
-                </div>
-            </div>
+  return (
+    <div className="login-container">
+      <img className="login-logo" src={LogoIcon} />
+      <div className="login-leftside">
+        <img className="login-back" src={LoginImage} />
+      </div>
+      <div className="login-rightside">
+        <div className="login-form">
+          <h1 className="login-heading">Welcome!</h1>
+          <p className="login-text">Enter details to login.</p>
+          <Input placeholder="Email" type="big" />
+          <div className="login-password">
+            <Input placeholder="Password" type="big-broken" />
+            <Link type="broken">SHOW</Link>
+          </div>
+          <Link type="normal">FORGOT PASSWORD?</Link>
+          <Button type="big" url="/users">
+            LOG IN
+          </Button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
